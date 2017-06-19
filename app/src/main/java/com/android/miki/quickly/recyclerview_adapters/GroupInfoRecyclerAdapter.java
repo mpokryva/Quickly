d8afd0192f0f94c3dd00bcd34de328d1ca8ac3dd
mpokryva <mpokryva@gmail.com>
2017-06-18 22:23:27 -0400

    Added card view layouts users and group name

--- app/src/main/java/com/android/miki/quickly/recyclerview_adapters/GroupInfoRecyclerAdapter.java
+++ app/src/main/java/com/android/miki/quickly/recyclerview_adapters/GroupInfoRecyclerAdapter.java
@@ -1,8 +1,28 @@
 package com.android.miki.quickly.recyclerview_adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.List;
+
 /**
  * Created by mpokr on 6/17/2017.
  */
 
-public class GroupInfoRecyclerAdapter {
+public class GroupInfoRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
